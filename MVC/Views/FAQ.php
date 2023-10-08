d57e39c64b5a9a18c5c7fc2f4cf7fa0dcc6ccc02
DangTienQuan <quan.dt.2294@aptechlearning.edu.vn>
2023-10-08 15:36:09 +0700

    FAQ_quan

--- MVC/Views/FAQ.php
+++ MVC/Views/FAQ.php
@@ -15,7 +15,7 @@
         .header{
             border-radius: 20px 20px 0px 0px;
             padding: 10px 0px;
-            background: purple;
+            background: #dca8a8;
             color: #fff;
             width: 100%;
             display: flex;
@@ -34,91 +34,195 @@
             width: 70%;
             margin: 0 auto;
         }
-        .faq-inner{
-            padding: 30px;
-            background: aliceblue;
-        }
-        .faq-plus{
-            float: right;
-            font-size: 1.4em;
-            line-height: 1em;
-            cursor: pointer;
-        }
-        hr{
-            background-color: #9b9b9b;
-        }
+    
     </style>
 </head>
 <body>
+<div class="header">
+    <nav class="navbar navbar-expand-lg " style="background-color: #dca8a8;">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><span class="text-warning h1">Mini</span>Luxe</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+          <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="#">HOME</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">BOOK NOW</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">SERVICES</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">NAIL ART</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PURE POLISH</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="#">PURE POLISH</a>
+            </li>
+            <li class="nav-item dropdown">
+              <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                MORE
+              </a>
+              <ul class="dropdown-menu">
+                <li><a class="dropdown-item" href="#">Gift Card</a></li>
+                <li><a class="dropdown-item" href="#">Policies</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Our Difference</a></li>
+              </ul>
+            </li>
+          </ul>
+          <div>
+            <ul class="nav">
+              <li class="nav-item">
+                  <a class="nav-link " href="#services">About Us</a>
+                </li>
+              <li class="nav-item">
+                <a href="" class="nav-link"
+                  ><i class="bi bi-person-circle"></i
+                ></a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link"><i class="bi bi-search"></i></a>
+              </li>
+              <li class="nav-item">
+                <a href="" class="nav-link"><i class="bi bi-bag"></i></a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </nav>
+    </div>
+    <br>
     <div class="container">
         <div class="row">
             <div class="faq-wrapper">
                 <div class="header">
                 <h1>FAQs</h1>
                 </div>
-                <div class="faq-inner">
-                    <div class="faq-item">
-                        <h3>
-                            What is an FAQ page?
-                            <span class="faq-plus">&plus;</span>
-                        </h3>
-                        <div class="faq-body">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere laboriosam dolores voluptatibus eius ullam voluptates consequatur atque nihil. Sequi suscipit molestias, aliquam amet veniam quae laboriosam praesentium accusantium enim hic!
+                <div class="accordion" id="accordionExample">
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                            WHY NAIL ART? OR WHY SHOULD I DO NAIL ART?
+                            </button>
+                        </h2>
+                        <div id="collapseOne" class="accordion-collapse collapse show" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            We talk about this a little bit in our bio, and although the reason is different for everyone, I think that the overarching sentiment is that it brings us joy (insert Marie Kondo here). Art is very emotional and I don’t think nail art is an exception. Every time I look down at my nails I feel a sense of pride, I’m proud to be making self-care a habit and proud of the beautiful work the artist was able to create. In a way it is also similar to why people get tattoos, they enjoy the art form and they want to use it to express themselves. Nail art is not permanent, so you can change it to reflect your state of mind month by month. The range of subjects the people have turned into nail art is outrageous. I’m sure after you try it for the first time you’ll be hooked.
+                            </div>
                         </div>
                     </div>
-                    <hr>
 
-                    <div class="faq-item">
-                        <h3>
-                            What is an FAQ page?
-                            <span class="faq-plus">&plus;</span>
-                        </h3>
-                        <div class="faq-body">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere laboriosam dolores voluptatibus eius ullam voluptates consequatur atque nihil. Sequi suscipit molestias, aliquam amet veniam quae laboriosam praesentium accusantium enim hic!
-                        </div>
-                    </div>
-                    <hr>
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                            WHAT IS NAIL ART PAINT/POWDER?
+                            </button>
+                        </h2>
+                        <div id="collapseTwo" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            Nail art is most commonly done in two mediums, acrylic or gel. 
 
-                    <div class="faq-item">
-                        <h3>
-                            What is an FAQ page?
-                            <span class="faq-plus">&plus;</span>
-                        </h3>
-                        <div class="faq-body">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere laboriosam dolores voluptatibus eius ullam voluptates consequatur atque nihil. Sequi suscipit molestias, aliquam amet veniam quae laboriosam praesentium accusantium enim hic!
-                        </div>
-                    </div>
-                    <hr>
+                            Acrylic is where you see techs dipping a brush into liquid and then into a powder to form a dough like clump. Acrylic comes in many different colors and is most commonly used to make extensions because it dries in a couple of seconds after the two substances are combined. 
+                            Hard gel can also be used to make extensions, and it is very similar to acrylic both chemically and physically. However, hard gel is a liquid, and stays liquid until it is cured under UV light. 
 
-                    <div class="faq-item">
-                        <h3>
-                            What is an FAQ page?
-                            <span class="faq-plus">&plus;</span>
-                        </h3>
-                        <div class="faq-body">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere laboriosam dolores voluptatibus eius ullam voluptates consequatur atque nihil. Sequi suscipit molestias, aliquam amet veniam quae laboriosam praesentium accusantium enim hic!
-                        </div>
-                    </div>
-                    <hr>
+                            Why technicians chose one or the other is usually personal preference. Why you, as the client, might have a preference is usually dependent on the removal process. Hard gel has a tighter chemical structure and can only be filed off, while acrylics can be soaked off with acetone.
 
-                    <div class="faq-item">
-                        <h3>
-                            What is an FAQ page?
-                            <span class="faq-plus">&plus;</span>
-                        </h3>
-                        <div class="faq-body">
-                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Facere laboriosam dolores voluptatibus eius ullam voluptates consequatur atque nihil. Sequi suscipit molestias, aliquam amet veniam quae laboriosam praesentium accusantium enim hic!
+                            You might be saying, I’ve had gel and it’s been soaked off! 
+
+                            This is soft gel, which is applied the same way as hard gel, but is engineered to have a weaker molecular structure that is penetrable by acetone. If you don’t want extensions, your tech will most likely use soft gel. They will then top coat the soft gel with hard gel, which is why during removal they file each nail a little first before they soak off. If your tech does not put a hard top coat over your soft gel, your nail might feel weak or the design will deteriorate, so keep a look out for this important last step. 
+
+                            Now you might be thinking, but what is dip powder then? As you might have suspected (if you have had a dip manicure before and know the process), dip powder is just a different way of applying acrylic. Dip is usually used for applying acrylic when you don’t need extensions. You can do nail art with dip powder, but to be honest it looks like a hassle compared to just doing it the regular way.  Also there have been some health and safety concerns with dip, as some salons will dip directly into the jar of powder. This means that they use the same jar for everyone and your finger could come into contact with germs that were on another client’s hands. If you want to try dip make sure they are pouring it on or using a separate bowl.                             </div>
                         </div>
                     </div>
-                    <hr>
-                </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                            HOW LONG WILL NAIL ART LAST?
+                            </button>
+                        </h2>
+                        <div id="collapseThree" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            NThe amount of time that a set will last is dependent on your type of nail, what products were used, and how it was applied. If applied correctly (on certain nail types) hard gels and acrylics can be basically immortal. We’ve seen evidence of a design lasting five weeks without damage. Soft gel designs that are topped with hard gel last, on average, three weeks before chipping and lifting occur. I’ve heard some people say that they could grow out their gel manicure to oblivion, attributing this to the expert application. Even at the best salon I’ve been to, my gel polish has lifted, so I think this is much more dependent on your nail type. Finally, manicures done with regular lacquer last 3.5 seconds before being totally not worth it. 
+                            </div>
+                        </div>
+                    </div>
+
+                    <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefour" aria-expanded="false" aria-controls="collapsefour">
+                            HOW MUCH DOES NAIL ART COST?
+                            </button>
+                        </h2>
+                        <div id="collapsefour" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            The short answer is $65 and up. The long answer is this it depends on where you go and what you ask for. If you know how much you want to spend you can just tell them to keep it at that price. If you know what design you want, show it to them before hand and ask them how much it will cost, then negotiate from there. If you are in a rich area *cough Manhattan* it’ll cost more because… economics. You don’t have to break the bank to get nail art. Just be clear and specific before you start your session.
+                            </div>
+                        </div>
+                      </div>
+
+                      <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsefive" aria-expanded="false" aria-controls="collapsefive">
+                            HOW LONG DOES IT TAKE TO GET NAIL ART?
+                            </button>
+                        </h2>
+                        <div id="collapsefive" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            I would block out at least two hours of your time for a nail art manicure. It could take less, my shortest appointment was an hour and a half, but better to be safe than sorry. If you know you are getting something complicated, like a raised shape or pattern, be prepared to sit for three plus hours.                            </div>
+                        </div>
+                      </div>
+
+                      <div class="accordion-item">
+                        <h2 class="accordion-header">
+                            <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapsesix" aria-expanded="false" aria-controls="collapsesix">
+                            CAN NAIL ART BE DONE WITH CND SHELLAC?                            </button>
+                        </h2>
+                        <div id="collapsesix" class="accordion-collapse collapse" data-bs-parent="#accordionExample">
+                            <div class="accordion-body">
+                            Shellac is the brand CND’s line of UV soft gel, so yes, it can totally be used for nail art! However, to my knowledge, Shellac on comes in bottles and not in pots. That may be why you don’t see it salons that use potted colors. Gels that come in pots or little jars give the techs more freedom and control. 
+
+</div>
+                        </div>
+                      </div>
+                  </div>
             </div>
         </div>
     </div>
-    <script type="text/javascript">
-        $(".faq-plus").on('click', function(){
-            $(this).parent().parent().find('.faq-body').slideToggle();
-        });
-    </script>
+</div>
+    </div>
+    <div class="footer">
+    <footer class="py-3 my-4 " style="background-color: #dca8a8;">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">BOOK NOW</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">SERVICES</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">NAIL ART</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">NAIL CARE</a>
+        </li>
+        <li class="nav-item">
+          <a href="#" class="nav-link px-2 text-muted">ABOUT</a>
+        </li>
+      </ul>
+      <p class="text-center text-muted">
+        58 Lê Thanh Nghị, Hai Bà Trưng, Hà Nội
+      </p>
+      <p class="text-center text-muted">© 2023 Miniluxe, Inc</p>
+    </footer>
+    </div>
 </body>
 </html>
