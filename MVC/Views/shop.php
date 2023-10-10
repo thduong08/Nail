d67c92e78561a2dafc250a6fbd0564b463138eff
Nguyen Hoang Anh <anhltvp2003@gmail.com>
2023-10-10 14:24:52 +0700

    update shop

--- MVC/Views/shop.php
+++ MVC/Views/shop.php
@@ -28,7 +28,7 @@ $resultProducts = $conn->query($sqlProducts);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Home</title>
+  <title>Shop</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.1/font/bootstrap-icons.css" />
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" />
