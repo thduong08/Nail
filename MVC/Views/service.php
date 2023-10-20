f924d97adac6f7df9d29779aea94bc8e3d98e415
nguyen van thi <nguyenvthi.95@gmail.com>
2023-10-20 11:22:16 +0700

    thi-add_service

---
+++ MVC/Views/service.php
@@ -0,0 +1,93 @@
+<!-- 1 -->
+<div class="mani card mb-3 py-3" style="max-width: 100%;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="../../img/image/nail1.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Mani</h5>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Time</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">1</th>
+      <td>Get mani</td>
+      <td>55$</td>
+      <td>45p</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Mani</td>
+      <td>25$</td>
+      <td>30p</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Mani polish change</td>
+      <td>15$</td>
+      <td>15p</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Dip powder mani</td>
+      <td>60$</td>
+      <td>60p</td>
+    </tr>
+
+  </tbody>
+</table>
+      </div>
+    </div>
+  </div>
+</div>
+<!-- 2 -->
+<div class="pedi card mb-3 py-3" style="max-width: 100%;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="../../img/image/nail1.jpg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Mani</h5>
+        <table class="table">
+  <thead>
+    <tr>
+      <th scope="col">#</th>
+      <th scope="col">Name</th>
+      <th scope="col">Price</th>
+      <th scope="col">Time</th>
+    </tr>
+  </thead>
+  <tbody class="table-group-divider">
+    <tr>
+      <th scope="row">1</th>
+      <td>Get pedi</td>
+      <td>72$</td>
+      <td>45p</td>
+    </tr>
+    <tr>
+      <th scope="row">2</th>
+      <td>Pedi</td>
+      <td>52$</td>
+      <td>30p</td>
+    </tr>
+    <tr>
+      <th scope="row">3</th>
+      <td>Pedi polish change</td>
+      <td>20$</td>
+      <td>15p</td>
+    </tr>
+  </tbody>
+</table>
+      </div>
+    </div>
+  </div>
+</div>
